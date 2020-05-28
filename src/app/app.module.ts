8af61e4c5255fc302e9c2dc70f4a476951b55a50
akifarfien <akif642@gmail.com>
2020-05-28 17:12:50 +0530

    router enabled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path : 'login', component: LoginFormComponent  },
+      {path:'', redirectTo:'login', pathMatch:'full'}
+      ]) 
   ],
   providers: [],
   bootstrap: [AppComponent]
